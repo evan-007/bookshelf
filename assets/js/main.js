b74079d3d7684f33441c80093c01d6b9b4a85af2
Evan Lloyd <evan.u.lloyd@gmail.com>
2017-12-22 09:41:59 -0500

    Use preset-env w/shipped proposals

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 const main = () => {
  console.log('hi');
+ const cat = { 1: 2 };
+ console.log({ ...cat });
 }
 
 export default main;
--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -3,5 +3,24 @@ module.exports = {
   output: {
     path: __dirname + "/../priv/static/js",
     filename: "app.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            "presets": [
+              ['@babel/preset-env', {
+                "shippedProposals": true
+              }]
+            ]
+          }
+        }
+      }
+    ]
+
   }
 };
